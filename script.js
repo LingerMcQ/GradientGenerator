fbbc4b7c3fccfa0486dd37381ce9e34957cfd972
Laurent Lin <t01078811@gmail.com>
2019-03-21 11:18:01 +0800

    updating new version 20190321

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    // info box variables
+    let colorLeft_INPUT = document.querySelector('.input__colorLeft');
+    let colorRight_INPUT = document.querySelector('.input__colorRight');
 
-	var colorHexNumber = document.querySelector("h3");
-	var color1 = document.querySelector(".color1");
-	var color2 = document.querySelector(".color2");
-	var randomChangeBoth = document.querySelector('.randomChangeBoth')
-	var randomChangeLeft = document.querySelector('.randomChangeLeft')
-	var randomChangeRight = document.querySelector('.randomChangeRight')
-	var body = document.getElementById("gradient");
+    let colorLeft_HEX = document.querySelector('.info__hexLeft');
+    let colorRight_HEX = document.querySelector('.info__hexRight');
+    let colorLeft_RGB = document.querySelector('.info__rgbLeft');
+    let colorRight_RGB = document.querySelector('.info__rgbRight');
 
+    // buttons variables
+    const randomChangeBoth = document.querySelector('.randomChangeBoth')
+	const randomChangeLeft = document.querySelector('.randomChangeLeft')
+    const randomChangeRight = document.querySelector('.randomChangeRight')
+    
+    // gradient variable
+    let gradient = document.querySelector('.gradient')
+    
+
+
+
+
+
+
+    // // initial Gradient
 	startGradient();
 
 	function startGradient() {
 		randomChangeColorLeft();
 		randomChangeColorRight();
-		body.style.background = 
-		"linear-gradient(to right, " 
-		+ color1.value 
-		+ ", " 
-		+ color2.value 
-		+ ")";
-	}
+        setGradient();
+    }
 
+    // set Gradient css
 	function setGradient() {
-		body.style.background = 
+		gradient.style.background = 
 		"linear-gradient(to right, " 
-		+ color1.value 
+		+ colorLeft_INPUT.value 
 		+ ", " 
-		+ color2.value 
+		+ colorRight_INPUT.value 
 		+ ")";
 
-		colorHexNumber.textContent = color1.value + "->" + color2.value ;
+        colorLeft_HEX.innerHTML = colorLeft_INPUT.value;
+        colorRight_HEX.innerHTML = colorRight_INPUT.value;
+        colorLeft_RGB.innerHTML = hex2rgb(colorLeft_INPUT.value);
+        colorRight_RGB.innerHTML = hex2rgb(colorRight_INPUT.value);
 	}
-	
+    
+    	// change color
 	function randomColor() {
 		let result = [];
 		for(i=1; i<=6; i++) {
@@ -43,25 +58,46 @@ document.addEventListener("DOMContentLoaded", () => {
 	}
 
 	function randomChangeColorLeft() {
-		color1.value = randomColor();
+		colorLeft_INPUT.value = randomColor();
 		setGradient();
 	}
 
 	function randomChangeColorRight() {
-		color2.value = randomColor();
+		colorRight_INPUT.value = randomColor();
 		setGradient();
-	}
+    }
+    
+
+    // hex2rgb function
+    function hex2rgb( colour ) {
+        var r,g,b;
+        if ( colour.charAt(0) == '#' ) {
+            colour = colour.substr(1);
+        }
+        if ( colour.length == 3 ) {
+            colour = colour.substr(0,1) + colour.substr(0,1) + colour.substr(1,2) + colour.substr(1,2) + colour.substr(2,3) + colour.substr(2,3);
+        }
+        r = colour.charAt(0) + '' + colour.charAt(1);
+        g = colour.charAt(2) + '' + colour.charAt(3);
+        b = colour.charAt(4) + '' + colour.charAt(5);
+        r = parseInt( r,16 );
+        g = parseInt( g,16 );
+        b = parseInt( b ,16);
+        return 'rgb(' + r + ',' + g + ',' + b + ')';
+    }
 
 
+	// touch button
+	colorLeft_INPUT.addEventListener("input", setGradient);
 
-	color1.addEventListener("input", setGradient);
-
-	color2.addEventListener("input", setGradient);
+	colorRight_INPUT.addEventListener("input", setGradient);
 
 	randomChangeBoth.addEventListener("click", () => {
 		randomChangeColorLeft();
 		randomChangeColorRight();
 	});
 	randomChangeLeft.addEventListener("click", randomChangeColorLeft);
-	randomChangeRight.addEventListener("click", randomChangeColorRight);
+    randomChangeRight.addEventListener("click", randomChangeColorRight);
+    
+    
 })
